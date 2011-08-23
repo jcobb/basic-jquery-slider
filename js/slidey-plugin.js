ca11cd3a838a1fccb4c6952a2f14dc5b92b1c223
jcobb <john.cobb.05@gmail.com>
2011-08-23 20:55:12 +1000

    Now with horizontal sliding enabled

--- js/slidey-plugin.js
+++ js/slidey-plugin.js
@@ -11,17 +11,25 @@
  * August 2011
  */
 
+
+
 (function($){
 	$.fn.slidey = function(options) {
-      
-        var debug = 0;                  // Debug mode. chnage to 1 to enable.
-        debug ? console.log('debug mode on') : '';
+
+
+
+		/* ============================================================
+		
+		Plug-in initialisation. Define variables, settings, etc
+		
+		============================================================== */
 	
 		var settings = {};	
 		
 		var defaults = {
-			width: 700,					// Width of the slider container & slides
-			height: 300,				// Height of the slider container & slides
+			width: 			700,		// Width of the slider container & slides
+			height: 		300,		// Height of the slider container & slides
+			animType:		'fade',		// Animation type, slide or fade
 			rotationSpeed: 	4000,		// The speed of the image rotation
             fadeDuration:   800,        // The duration of the fade transition
 			child: 			'img',		// The type of element which we will be rotating
@@ -38,59 +46,135 @@
 		// Overwrite the defaults with the provided options (if any)
 		settings = $.extend({}, defaults, options);
 		
-		// non-jQuery vars
-		var paused = false,				// Is the slider currently paused?
-			fwd = 0,					// Value indicating next
-			back = -1,					// Value indicating previous
-			current_slide = 1,			// The currently active slide
-			marker_pos = 0,				// The currently active marker
-            animating = false           // Is the slider currently animating?
-			
-		// jQuery vars
-		var	$slider = this,									// The slider parent
-			$banners = $slider.children(settings.child), 	// The slides
-			$position,										// Marker wrapper
-			$markers,										// Markers
-			$next,											// Next button
-			$previous										// Previous button
-		
+		// Variables
+		var	$slider = this,	
+			banners = $slider.children(settings.child),
+			slideCount = banners.length,
+			paused = false,
+			animating = false,
+			fwd = 0,
+			back = -1,
+			current_slide = 1,
+			marker_pos = 0
+
+		// Set the height and width of the slider wrapper and slider
 		$($slider).height(settings.height).width(settings.width);
-		$($banners).height(settings.height).width(settings.width);
+		$(banners).height(settings.height).width(settings.width);
 		
-		// Hook me with some positional indicators!	
+		
+		/* ============================================================
+		
+		Prepare the elements for a horizontal slider
+		
+		============================================================== */
+		
+		if(settings.animType == 'slide'){
+			
+			// Get the first and last slides
+			$first = $(banners[0]);
+			$last = $(banners[banners.length-1]);
+			
+			// Clone them, then add the first to the back
+			// and the last to the front so we can do some voodoo
+			$last.clone().prependTo($slider);
+			$first.clone().appendTo($slider);
+			
+			// Update our array of slides
+			banners = $slider.children(settings.child)
+			
+			// Make a canvas that will hold the slides
+			$canvas = $('<div id="canvas"></div>');
+
+			// Set the width of the canvas based on number of slides and settings
+			$canvas.width(banners.length*settings.width).height(settings.height)
+			
+			$slider.css('overflow','hidden');
+			
+			$canvas.css({
+				'position' : 'absolute',
+				'left' : -settings.width + 'px'
+			});
+			
+			banners.css({
+				'float' : 'left',
+				'position' : 'relative'
+			})
+			
+			// Put it all back together
+			$canvas.append(banners);
+			$slider.append($canvas);
+			
+			current_slide =2;
+			
+		}
+		
+		// If it's a fade animation, we don't need to do much at all
+		
+		if(settings.animType == 'fade'){
+			$(banners).addClass('slide');              // Give all banners the slide class
+			$(banners[0]).addClass('active-slide');    // Set the first banner to active
+		}
+		
+		
+		
+		
+		/* ============================================================
+		
+		If markers are enabled, create them and add them
+		
+		============================================================== */
+		
 		if(settings.showMarkers){
 		
-			// Create the parent element and append it to the slider
-			$position = $('<ul class="markers"></ul>');	
-			$position.appendTo($slider);
+			// Create the wrapper
+			$marker_wrapper = $('<ul class="markers"></ul>');
+			$marker_wrapper.appendTo($slider);
 			
-			$.each($banners,function(key,value){
+			//Create a marker for each banner and add append it to the wrapper
+			$.each(banners,function(key,value){
 				key++
-				$('<li><a href="#">'+key+'</a></li>').appendTo($position);
+				if(settings.animType == 'slide'){
+					if(key != 1 && key != banners.length)
+						$('<li><a href="#">'+key+'</a></li>').appendTo($marker_wrapper);
+				}
+				else{
+					$('<li><a href="#">'+key+'</a></li>').appendTo($marker_wrapper);
+				}
 			});
 
-			// Gather all the markers
-			$markers = $position.children('li');
+			// Gather all the markers up in to an array
+			markers = $marker_wrapper.children('li');
 		
 			// Iterate over the markers and bind a click event to each one
-			$.each($markers, function(key,value){
+			$.each(markers, function(key,value){
 				$(value).click(function(e){
 					e.preventDefault();
                     if(!$(this).hasClass('active-marker') && !animating)
-                      slideyGo(key+1);
+						slideyGo(key+1);
 				});
 			})
 
 			// Horizontally center the markers
 			if(settings.centerMarkers){
-				offset = ($slider.innerWidth() - $position.innerWidth() )/ 2;
-				$position.css('left', offset);
+				offset = (settings.width - $marker_wrapper.width() )/ 2;
+				$marker_wrapper.css('left', offset);
 			}
 
-			$($markers[0]).addClass('active-marker');
+			// Set the first one as active
+			$(markers[0]).addClass('active-marker');
+			
 		}
 
-		// Hook up the controls!
+		
+		
+		
+		
+		/* ============================================================
+		
+		If controls are enabled, create them and add them
+		
+		============================================================== */
+		
 		if(settings.showControls){
 			
 			// Create the control elements and append them to the slider
@@ -104,13 +188,13 @@
 			$next.click(function(e){
 				e.preventDefault();
                 if(!animating)
-                  slideyGo(fwd);
+					slideyGo(fwd);
 			});
 			
 			$previous.click(function(e){
 				e.preventDefault();
                 if(!animating)
-                  slideyGo(back);
+					slideyGo(back);
 			});
 			
 			// Vertically center the controllers
@@ -122,105 +206,224 @@
 	
 		}
 		
-		//If pause on hover is enabled, we need to hook that shit up!
+
+
+
+
+		/* ============================================================
+		
+		Enable pause on hover only if autoRotate is also enabled
+		
+		============================================================== */
+		
 		if(settings.hoverPause && settings.autoRotate){
 			
 			$slider.hover(function(){
-				
 				if(!paused){
 					clearInterval(slideyInterval);
 					paused=true;
 				}
-				
 			},function(){
-				
 				if(paused){
 					slideyInterval = setInterval(function(){ slideyGo(fwd) }, settings.rotationSpeed);
 					paused=false;
 				}
-				
 			});
 			
 		}
-		
-        $($banners).addClass('slide');              // Give all banners the slide class
-        $($banners[0]).addClass('active-slide');    // Set the first banner to active
         
+		
+		
+		
+		/* ============================================================
+		
+		Kick off the rotation if autoRotate is enabled
+		
+		============================================================== */
+		
 		if(settings.autoRotate){
           var slideyInterval = setInterval(function(){ slideyGo(fwd) }, settings.rotationSpeed);
         }
 		
-		// Go Slidey, Go!
+		
+		
+		
+		/* ============================================================
+		
+		The function that makes the magic happen
+		
+		============================================================== */
+		
 		var slideyGo = function(move){
           
+			// We're animating!
             animating=true;
 			
-			var child = settings.child,
-				position;
-			
-			$.each($banners,function(key,value){
+			// If we're doing a fade animation...
+			if(settings.animType == 'fade'){
 				
-				$element = $(value);
-		
-				if($element.hasClass('active-slide')) {
-					$active = $element;
-					position = key;
-				}
+				var child = settings.child,
+					current_slide;
 				
-			});
-
-			$active.stop();
+				$.each(banners,function(key,value){
+					
+					$element = $(value);
 			
-			if(move==fwd){
+					if($element.hasClass('active-slide')) {
+						$active = $element;
+						current_slide = key;
+					}
+					
+				});
+				
 				
 				// Forza Slidey!
-				
-				// Do we need to loop to the start?
-				if(!$active.next(child).length){
-					$next = $slider.children(child+':first');
-					marker_pos = 0;
+				if(move==fwd){
+					
+					// Do we need to loop to the start?
+					if(!$active.next(child).length){
+						$next = $slider.children(child+':first');
+						marker_pos = 0;
+					}
+					else{
+						$next = $active.next(child)
+						marker_pos = current_slide+1;
+					}
+				}
+				else if(move==back){
+					
+					// Back slidey! Go back!!
+					
+					// Do we need to loop to the end?
+					if(!$active.prev(child).length){
+						$next = $slider.children(child+':last');
+						marker_pos = markers.length-1;
+					}
+					else{
+						$next = $active.prev(child)
+						marker_pos = current_slide-1;
+					}
 				}
 				else{
-					$next = $active.next(child)
-					marker_pos = position+1;
+					
+					// Jump around slidey! Jump up, Jump up and get down!
+					$next = $(($slider.children(child)[move-1]));
+					marker_pos = move-1;
+					
 				}
-			}
-			else if(move==back){
 				
-				// Back slidey! Go back!!
-				
-				// Do we need to loop to the end?
-				if(!$active.prev(child).length){
-					$next = $slider.children(child+':last');
-					marker_pos = $markers.length-1;
+				if(settings.showMarkers){
+					markers.removeClass('active-marker');
+					$(markers[marker_pos]).addClass('active-marker');
 				}
-				else{
-					$next = $active.prev(child)
-					marker_pos = position-1;
-				}
-			}
-			else{
 				
-				// Jump around slidey! Jump up, Jump up and get down!
-				$next = $(($slider.children(child)[move-1]));
-				marker_pos = move-1;
-				
-			}
+				//Give the active elements the last-active class to give them a lower z-index than the next elements to be displayed
+				$active.addClass('last-active-slide');
 			
-			if(settings.showMarkers){
-				$markers.removeClass('active-marker');
-				$($markers[marker_pos]).addClass('active-marker');
+				//Animate the opacity of the next image to be displayed and give it the '.active' class to bring it to the front
+				$next.css({opacity: 0.0}).addClass('active-slide').animate({opacity: 1.0}, 500, function() {
+					$active.removeClass('active-slide last-active-slide');
+					animating =false;
+				});
+				
 			}
+		
+		
+			// Horizontal slider		
+			if(settings.animType == 'slide'){
+				
+				console.log('starting slide....');
 			
-			//Give the active elements the last-active class to give them a lower z-index than the next elements to be displayed
-			$active.addClass('last-active-slide');
-		
-			//Animate the opacity of the next image to be displayed and give it the '.active' class to bring it to the front
-			$next.css({opacity: 0.0}).addClass('active-slide').animate({opacity: 1.0}, 500, function() {
-				$active.removeClass('active-slide last-active-slide');
-                animating =false;
-			});
-		
+				position = $canvas.position();
+				
+				current_slide = ((position.left/settings.width)*-1);
+				
+				console.log('	starting pos: '+current_slide);
+				
+				if(move == 0){
+					
+					new_position = position.left - settings.width;
+					
+					$canvas.animate({'left': new_position}, 500, function(){
+						
+						if(new_position == -(settings.width * (slideCount + 1))){
+							$canvas.css('left',-settings.width);
+							current_slide=1;
+							console.log('		next-if: '+current_slide);
+						}
+						else{
+							current_slide++;
+							console.log('		next-else: '+current_slide);
+						}
+						
+						if(settings.showMarkers){
+							markers.removeClass('active-marker');
+							$(markers[current_slide-1]).addClass('active-marker');
+						}
+						
+						animating=false;
+						console.log('	ending pos: '+current_slide);
+						
+					});
+
+				}else if(move == -1){
+					
+					new_position = position.left + settings.width;
+					
+					$canvas.animate({'left': new_position}, 500, function(){
+						
+						if(new_position == 0){
+							$canvas.css('left',-settings.width*slideCount);
+							current_slide=slideCount;
+							console.log('		prev-if: '+current_slide);
+						}
+						else{
+							current_slide--;
+							console.log('		prev-else: '+current_slide);
+						}
+						
+						if(settings.showMarkers){
+							markers.removeClass('active-marker');
+							$(markers[current_slide-1]).addClass('active-marker');
+						}
+						
+						animating=false;
+						console.log('	ending pos: '+current_slide);
+						
+					});
+
+				}else{
+					
+					new_position = -(move * settings.width);
+					
+					$canvas.animate({'left': new_position}, 500, function(){
+						
+						if(new_position == 0){
+							$canvas.css('left',-settings.width*slideCount);
+							$current_slide=slideCount-1;
+						}
+						
+						if(new_position == -(settings.width * (slideCount + 1))){
+							$canvas.css('left',-settings.width);
+							current_slide=1;
+						}
+						else{
+							current_slide=move;
+						}
+						
+						if(settings.showMarkers){
+							markers.removeClass('active-marker');
+							$(markers[current_slide-1]).addClass('active-marker');
+						}
+						
+						animating=false;
+						console.log('	ending pos: '+current_slide);
+						
+					});
+					
+				}
+				
+			}
 			
 		}
 		
